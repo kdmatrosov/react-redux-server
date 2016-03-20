670608b23d2725a478a69affa83fe5db96b41a26
Kirill Matrosov <kdmatrosov@gmail.com>
2016-03-20 16:12:31 +0300

    add jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,11 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({sub: user.id, iat: timestamp}, config.secret); // у jwt есть свойство sub (subject)
+}
 
 exports.signup = function(req, res, next)
 {
@@ -32,7 +39,7 @@ exports.signup = function(req, res, next)
             return next(err);
           }
           // Ответ, что пользователь создан
-          res.json(user);
+          res.json({token: tokenForUser(user)});
       });
 
   });
